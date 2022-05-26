dc2a715fd1b03901c14c48756eb4980bd7b47766
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-05-26 10:06:25 -0700

    comparison feature worked out

--- autocomplete.js
+++ autocomplete.js
@@ -1,6 +1,12 @@
-const createAutocomplete = ({ root }) => {
+const createAutocomplete = ({ 
+    root, 
+    renderOption, 
+    onOptionSelect, 
+    inputValue, 
+    fetchData 
+    }) => {
     root.innerHTML = `
-    <label><b>Search for a Movie</b></label>
+    <label><b>Search</b></label>
     <input class="input" />
     <div class="dropdown">
         <div class="dropdown-menu">
@@ -13,31 +19,24 @@ const createAutocomplete = ({ root }) => {
     const dropdown = root.querySelector('.dropdown');
     const resultsWrapper = root.querySelector('.results');
 
-    let timeoutId;
-
     const onInput = async event => {
-        const movies = await fetchData(event.target.value);
-        if (!movies.length) {
+        const items = await fetchData(event.target.value);
+        if (!items.length) {
             dropdown.classList.remove('is-active');
             return;
         }
-        
         resultsWrapper.innerHTML = '';
-
         dropdown.classList.add('is-active');
-        for (let movie of movies) {
+        for (let item of items) {
             const option = document.createElement('a');
-            const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+           
 
             option.classList.add('dropdown-item');
-            option.innerHTML = `
-            <img src="${imgSrc}" />
-            ${movie.Title}
-            `;
+            option.innerHTML = renderOption(item);
             option.addEventListener('click', () => {
                 dropdown.classList.remove('is-active');
-                input.value = movie.Title;
-                onMovieSelect(movie);
+                input.value = inputValue(item);
+                onOptionSelect(item);
             })
             resultsWrapper.appendChild(option);
         }
